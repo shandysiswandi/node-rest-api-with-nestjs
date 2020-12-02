1469534207508be231e16fb56c7059bfd4e4e0a2
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-02 23:16:10 +0700

    comment csrf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as helmet from 'helmet';
 import * as rateLimit from 'express-rate-limit';
-import * as csurf from 'csurf';
+// import * as csurf from 'csurf';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -17,7 +17,7 @@ async function bootstrap() {
       max: 60, // limit each IP to 60 requests per windowMs
     }),
   );
-  app.use(csurf());
+  // app.use(csurf());
 
   // listen
   await app.listen(process.env.PORT);
