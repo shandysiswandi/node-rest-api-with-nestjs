4768e29c4cd66531621a6af073d1cb840e69cbfb
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-02 22:18:48 +0700

    add middleware, cors, helmet, csrf, and ratelimit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as csurf from 'csurf';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // instance application
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // middlewares
+  app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 60 * 1000, // 1 minute
+      max: 60, // limit each IP to 60 requests per windowMs
+    }),
+  );
+  app.use(csurf());
+
+  // listen
+  await app.listen(process.env.PORT);
 }
 
 bootstrap();
