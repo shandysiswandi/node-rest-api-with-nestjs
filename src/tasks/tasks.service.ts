0ed213c02731360177e1a5d647bea43858b9d2bd
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-03 10:03:59 +0700

    add method getTasksWithFilters & updateFieldTaskById

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter.dto';
+import { UpdateFieldTaskDto } from './dto/update-field-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,6 +12,25 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+
+  getTasksWithFilters(tasksFilter: GetTaskFilterDto): Task[] {
+    const { search, status } = tasksFilter;
+
+    let tasks = this.tasks;
+
+    if (search) {
+      tasks = this.tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+
+    if (status) {
+      tasks = this.tasks.filter((task) => task.status === status);
+    }
+
+    return tasks;
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -28,11 +49,17 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found.`);
+    }
+
+    return task;
   }
 
   updateTaskById(id: string, updateTaskDto: UpdateTaskDto): Task {
-    const task = this.tasks.find((task) => task.id === id);
+    const task = this.getTaskById(id);
 
     task.title = updateTaskDto.title;
     task.description = updateTaskDto.description;
@@ -41,7 +68,30 @@ export class TasksService {
     return task;
   }
 
+  updateFieldTaskById(
+    id: string,
+    updateFieldTaskDto: UpdateFieldTaskDto,
+  ): Task {
+    const task = this.getTaskById(id);
+
+    if (updateFieldTaskDto.title) {
+      task.title = updateFieldTaskDto.title;
+    }
+
+    if (updateFieldTaskDto.description) {
+      task.description = updateFieldTaskDto.description;
+    }
+
+    if (updateFieldTaskDto.status) {
+      task.status = updateFieldTaskDto.status;
+    }
+
+    return task;
+  }
+
   deleteTaskById(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const taskData = this.getTaskById(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== taskData.id);
   }
 }
