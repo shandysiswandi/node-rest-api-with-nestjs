c6a0375e6f173c6b99bb022cef493f673d8b006d
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-04 01:49:32 +0700

    call middlewares function

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
-import * as helmet from 'helmet';
-import * as rateLimit from 'express-rate-limit';
-// import * as csurf from 'csurf';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './application/app.module';
+import { middlewares } from './misc/middleware';
 
 async function bootstrap() {
-  // instance application
   const app = await NestFactory.create(AppModule);
 
-  // middlewares
-  app.enableCors();
-  app.use(helmet());
-  app.use(
-    rateLimit({
-      windowMs: 60 * 1000, // 1 minute
-      max: 60, // limit each IP to 60 requests per windowMs
-    }),
-  );
-  // app.use(csurf());
+  middlewares(app);
 
-  // listen
   await app.listen(process.env.APP_PORT);
 }
 
