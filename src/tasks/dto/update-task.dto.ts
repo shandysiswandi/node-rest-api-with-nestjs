8cd0432f0bcfd4ade3a4c4afcae6bd00d142a5f8
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-03 10:04:42 +0700

    add validation

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,16 @@
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class UpdateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
 }
