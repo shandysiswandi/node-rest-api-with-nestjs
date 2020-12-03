f8bd100379107f8c231dfd2c95074cd7af73661b
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-04 01:49:47 +0700

    create middlewares func

---
+++ src/misc/middleware.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+// import * as csurf from 'csurf';
+
+export function middlewares(app: INestApplication): void {
+  app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 60 * 1000, // 1 minute
+      max: 60, // limit each IP to 60 requests per windowMs
+    }),
+  );
+  // app.use(csurf());
+}
