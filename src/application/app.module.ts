89866b06a418f285009b4595d0544af4ddce1f00
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-04 01:54:19 +0700

    fix config typeorm import & added config to global

---
+++ src/application/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from '../config/typeorm.config';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    TasksModule,
+  ],
+})
+export class AppModule {}
