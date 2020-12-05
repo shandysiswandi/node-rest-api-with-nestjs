453de6b74196992ed50739aabb5ea5da35bdc965
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-06 05:21:25 +0700

    inject user repo

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../repositories/user.repository';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  //
+}
