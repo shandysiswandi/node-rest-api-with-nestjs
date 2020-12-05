91e0e142693515ba829e9975893e8923cfabc327
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-06 05:21:46 +0700

    import user repo

--- src/app/modules/users.module.ts
+++ src/app/modules/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from '../services/users.service';
 import { UsersController } from '../controllers/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserRepository } from '../repositories/user.repository';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [UsersController],
   providers: [UsersService],
 })
