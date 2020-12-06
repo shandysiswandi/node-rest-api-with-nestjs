202281d8a6a55caafaa88596c76da5f96f8b3980
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-06 11:54:31 +0700

    add httpcode

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -1,21 +1,22 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { RegisterDto } from '../dto/auth/register.dto';
+import { User } from 'src/entities/user.entity';
+import { LoginDto } from '../dto/auth/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  /**
-   * Auth register
-   *
-   * @Route POST /auth/register
-   * @param registerDto RegisterDto
-   * @Code 201
-   * @return Task
-   */
+  @Post('login')
+  @HttpCode(200)
+  login(@Body() loginDto: LoginDto): Promise<User> {
+    return this.authService.login(loginDto);
+  }
+
   @Post('register')
-  createTask(@Body(ValidationPipe) registerDto: RegisterDto): Promise<void> {
+  @HttpCode(201)
+  register(@Body() registerDto: RegisterDto): Promise<User> {
     return this.authService.register(registerDto);
   }
 }
