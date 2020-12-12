c189f426ebe9611c8ccf8d44cda63231912d7fd8
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:47:42 +0700

    change dir

--- src/common/interceptor.ts
+++ src/common/interceptor.ts
@@ -1,5 +1,5 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from 'src/app/interceptor/response.interceptor';
+import { ResponseInterceptor } from '../interceptors/response.interceptor';
 
 export function interceptors(app: INestApplication): void {
   app.useGlobalInterceptors(new ResponseInterceptor());
