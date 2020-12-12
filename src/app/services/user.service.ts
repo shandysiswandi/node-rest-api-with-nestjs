df158009883b6bdd0a724128d83a1389e71c0167
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 22:15:26 +0700

    update user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from '../repositories/user.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectRepository(UserRepository) private readonly userRepo: UserRepository) { }
+}
