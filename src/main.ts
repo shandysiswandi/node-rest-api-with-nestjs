fcd10059b6d3c4562b61427383160b81912458ef
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:10:39 +0700

    add interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { middlewares, validations } from './common';
+import { interceptors, middlewares, validations } from './common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  /**
-   * Call list of middleware
-   */
+  // Call list of global middleware
   middlewares(app);
 
-  /**
-   * Call list of validation-pipe
-   */
+  // Call list of global validation-pipe
   validations(app);
+
+  // Call list of global interceptor
+  interceptors(app);
 
   await app.listen(process.env.APP_PORT, () => {
     if (process.env.APP_ENV === 'local') {
