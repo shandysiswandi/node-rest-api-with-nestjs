2b9172bddf76eeb1f448aacfec2f0b0d7d870dd1
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 22:30:19 +0700

    change import path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './modules/task.module';
-import { AuthModule } from './modules/auth.module';
-import { UsersModule } from './modules/user.module';
+import { TasksModule } from 'src/app/task/task.module';
+import { AuthModule } from 'src/app/auth/auth.module';
+import { UsersModule } from 'src/app/user/user.module';
 import typeOrmConfig from 'src/config/typeorm.config';
 
 @Module({
