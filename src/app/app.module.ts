b2de2abf40d9ffa5d99c4047334df0e16461745c
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:11:22 +0700

    update TypeOrmModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from 'src/config/typeorm.config';
 import { TasksModule } from './modules/tasks.module';
 import { AuthModule } from './modules/auth.module';
 import { UsersModule } from './modules/users.module';
+import typeOrmConfig from 'src/config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(typeOrmConfig),
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => typeOrmConfig(config),
+    }),
     AuthModule,
     UsersModule,
     TasksModule,
   ],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
