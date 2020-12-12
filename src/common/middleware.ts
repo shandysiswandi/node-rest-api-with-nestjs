77d19f5c3e8fa5679fed74038b0d158713bfe863
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:13:30 +0700

    update rateLimit message

--- src/common/middleware.ts
+++ src/common/middleware.ts
@@ -6,11 +6,22 @@ import * as helmet from 'helmet';
 export function middlewares(app: INestApplication): void {
   app.enableCors();
   app.use(helmet());
-  app.use(
-    rateLimit({
-      windowMs: 60 * 1000, // 1 minute
-      max: 60, // limit each IP to 60 requests per windowMs
-    }),
-  );
+  app.use(rateLimitFunc());
   // app.use(csurf());
+}
+
+// For limit incomming request
+// windowMs : number  => batas waktu limit request (miliseconds)
+// max      : number  => limit each IP to 60 requests per windowMs
+// message  : object  => custom response message
+function rateLimitFunc() {
+  return rateLimit({
+    windowMs: 60 * 1000,
+    max: 60,
+    message: {
+      status: 429,
+      message: 'You have reached the limit request.',
+      error: 'Too many requests, please try again later.',
+    },
+  });
 }
