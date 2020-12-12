607429cbc08fcf0d5bb9d21551843013bc4b9424
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:42:15 +0700

    move dir interceptors to src

---
+++ src/interceptors/response.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Response } from '../types/response.interface';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(_: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((result) => ({
+        data: result,
+      })),
+    );
+  }
+}
