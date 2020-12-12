0b91729c4ee2e5cd90f4d11d06e11dcbf544a959
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 22:14:33 +0700

    rename file in modlues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TasksModule } from './modules/tasks.module';
+import { TasksModule } from './modules/task.module';
 import { AuthModule } from './modules/auth.module';
-import { UsersModule } from './modules/users.module';
+import { UsersModule } from './modules/user.module';
 import typeOrmConfig from 'src/config/typeorm.config';
 
 @Module({
