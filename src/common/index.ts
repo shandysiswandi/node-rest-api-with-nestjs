3bba2dc7827047c83df99e86d76163d1c12b550e
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 19:14:00 +0700

    update common

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,3 +1,5 @@
 export { hashBcrypt, compareBcrypt } from './bcrypt';
 export { middlewares } from './middleware';
 export { validations } from './validation';
+export { interceptors } from './interceptor';
+export { isCanDelete, isCanGetID, entityFields } from './check-data';
