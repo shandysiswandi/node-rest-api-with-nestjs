4ed74e8c1f3b534d31baa36d5db6fe5c57efd13d
Shandy Siswandi <shandysiswandi@gmail.com>
2020-12-12 22:11:38 +0700

    create task payload

---
+++ src/app/payloads/task.payload.ts
@@ -0,0 +1,43 @@
+import { IsIn, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { TaskStatus } from '../../types/task-status.enum';
+
+// CreateTaskPayload
+export class CreateTaskPayload {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+}
+
+// UpdateTaskPayload
+export class UpdateTaskPayload {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+
+    @IsNotEmpty()
+    @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+    status: TaskStatus;
+}
+
+// UpdateFieldTaskPayload
+export class OptionalFieldTaskPayload {
+    @IsOptional()
+    @IsString()
+    title?: string;
+
+    @IsOptional()
+    @IsString()
+    description?: string;
+
+    @IsOptional()
+    @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
+    status?: TaskStatus;
+}
